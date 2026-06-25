33b236222784c0e95b0a3ad3d45f06f056e68fab
MadorTill <mador.till@gmail.com>
2026-06-25 08:53:43 +0300

    555

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,32 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   base: process.env.NODE_ENV === "production" ? "/lomdat-klia-Bahad13/" : "/",
+// })
+
+
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
+  // משתמש בנתיב של גיטהאב רק בזמן build (פרודקשן), ובסלאש רגיל בזמן dev במחשב
+  base: command === 'build' ? '/lomdat-klia-Bahad13/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: process.env.NODE_ENV === "production" ? "/lomdat-klia-Bahad13/" : "/",
-})
+  }
+}))
